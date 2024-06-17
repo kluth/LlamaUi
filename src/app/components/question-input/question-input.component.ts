f3197675065247445a9613ff7282fa1b28941b44
Matthias Kluth <kluthmatthias@gmail.com>
2024-06-17 15:27:48 +0200

    add input to root path

--- src/app/components/question-input/question-input.component.ts
+++ src/app/components/question-input/question-input.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-question-input',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule],
   templateUrl: './question-input.component.html',
   styleUrl: './question-input.component.scss'
 })
